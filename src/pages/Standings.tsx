cccccc198d931e945fa4565c3d515094965b66e8
Riya <mriya842@gmail.com>
2023-12-05 10:35:12 +0530

    routes

---
+++ src/pages/Standings.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface Props {
+  // Define your props here
+}
+
+const Standings: React.FC<Props> = (Props) => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default Standings;
